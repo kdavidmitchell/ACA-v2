a3dc8e93babc5963963b1503f3f35046bad7eb24
kdavidmitchell <kdavidmitchell@gmail.com>
2018-02-27 20:29:47 -0500

    Built character enhancement screen

---
+++ Assets/Scripts/Character Enhancement/CharacterEnhancementManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterEnhancementManager : MonoBehaviour 
+{
+
+	private bool _enhancementIsActive;
+	
+	public GameObject enhancementPanel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		enhancementPanel.SetActive(false);
+		_enhancementIsActive = true;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			_enhancementIsActive = !_enhancementIsActive;
+			enhancementPanel.SetActive(!_enhancementIsActive);
+		}	
+	}
+}
