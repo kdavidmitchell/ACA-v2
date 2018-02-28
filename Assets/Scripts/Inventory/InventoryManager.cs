45ebc232d2fb60c2399c35da9f3da7eb83dff6b5
kdavidmitchell <kdavidmitchell@gmail.com>
2018-02-27 19:12:09 -0500

    Built inventory window

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour 
+{
+
+	private bool _inventoryIsActive;
+	
+	public GameObject inventoryPanel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		inventoryPanel.SetActive(false);
+		_inventoryIsActive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			_inventoryIsActive = !_inventoryIsActive;
+			inventoryPanel.SetActive(!_inventoryIsActive);
+		}
+	}
+}
