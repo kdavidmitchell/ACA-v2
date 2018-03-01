c73eedf162b1e4569ef4d05c94541699c3afa180
kdavidmitchell <kdavidmitchell@gmail.com>
2018-03-01 15:23:18 -0500

    Trying to build battle scene

---
+++ Assets/Scenes/Scripts/BattleScreenManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleScreenManager : MonoBehaviour 
+{
+
+	private string _playerName;
+	private int _playerHealth;
+	private int _playerAmbition;
+	private List<BaseAbility> _playerAbilities = new List<BaseAbility>();
+
+	public Text playerName;
+	public Text enemyName;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		LoadInformation.LoadAllInformation();	
+		_playerName = GameInformation.PlayerName;
+		_playerHealth = GameInformation.PlayerClass.ClassStats[3].StatModifiedValue;
+		_playerAmbition = GameInformation.PlayerClass.ClassStats[4].StatModifiedValue;
+
+		playerName.text = _playerName;
+		//enemyName.text = _enemyName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NormalAttack()
+	{
+
+	}
+
+	public void ActiveAbility1()
+	{
+
+	}
+
+	public void OpenItemMenu()
+	{
+
+	}
+
+	public void Capitulate()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Battle System/InformationHoverHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InformationHoverHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+{
+
+	private GameObject instance;
+	
+	public GameObject hoverPrefab;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		instance = Instantiate(hoverPrefab, transform.position + new Vector3(250, 0, 0), Quaternion.identity);
+		instance.transform.parent = gameObject.transform;
+
+		Text hoverText = instance.GetComponentInChildren<Text>();
+		hoverText.text = Parse(name);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Destroy(instance);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		LoadInformation.LoadAllInformation();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private string Parse(string name)
+	{
+		string result = "";
+
+		if (name == "Attack_Button")
+		{
+			result = GameInformation.PlayerClass.ClassAbilities[0].AbilityName + ": " + GameInformation.PlayerClass.ClassAbilities[0].AbilityDescription + " Deals " +
+			GameInformation.PlayerClass.ClassAbilities[0].AbilityDamage[GameInformation.PlayerClass.ClassAbilities[0].AbilityCurrentRank - 1].ToString() + " damage, plus weapon damage.";
+		} else if (name == "Abilities_Button")
+		{
+			result = GameInformation.PlayerClass.ClassAbilities[1].AbilityName + ": " + GameInformation.PlayerClass.ClassAbilities[1].AbilityDescription + " Deals " +
+			GameInformation.PlayerClass.ClassAbilities[1].AbilityDamage[GameInformation.PlayerClass.ClassAbilities[1].AbilityCurrentRank - 1].ToString() + " damage.";
+		}
+
+		return result;
+	}
+}
