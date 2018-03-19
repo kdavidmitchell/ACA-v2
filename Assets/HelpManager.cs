0657ef50f262365e98ebe41c1aaf3ea08e9ce4d3
kdavidmitchell <kdavidmitchell@gmail.com>
2018-03-19 18:27:07 -0400

    Added help menu

---
+++ Assets/HelpManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpManager : MonoBehaviour 
+{
+	private bool _helpIsActive;
+
+	public GameObject helpPanel;
+	public static HelpManager instance = null;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		} else if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start()
+	{
+		helpPanel.SetActive(false);
+		_helpIsActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			_helpIsActive = !_helpIsActive;
+			helpPanel.SetActive(_helpIsActive);
+		}
+	}
+}
